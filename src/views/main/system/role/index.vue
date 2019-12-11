a1bdae256713c63aa0b2de713e11308bfb0af0bd
Naldo Wang <1165648703@qq.com>
2019-12-11 17:58:52 +0800

    角色页面编辑模块

--- src/views/main/system/role/index.vue
+++ src/views/main/system/role/index.vue
@@ -17,7 +17,7 @@
         <eg-button @click="searchClick">查询</eg-button>
       </template>
       <template v-slot:headerRight>
-        <eg-button @click="showEdit({isShow: true})">新建角色</eg-button>
+        <eg-button @click="showEdit()">新建角色</eg-button>
       </template>
       <template v-slot:content>
         <el-table :data="roleList">
@@ -28,7 +28,7 @@
           <el-table-column label="创建人" prop="CreatorName" align="center"/>
           <el-table-column label="操作" align="center">
             <template v-slot="{row}">
-              <eg-button type="text" style="margin-right: 1rem;">编辑</eg-button>
+              <eg-button type="text" style="margin-right: 1rem;" @click="showEdit({row})">编辑</eg-button>
               <eg-button type="text" color="danger">删除</eg-button>
             </template>
           </el-table-column>
@@ -55,7 +55,8 @@
       <template v-slot:content>
         <div class="role-edit__row">
           <label class="role-edit__row-title">角色名称</label>
-          <eg-input width-type="medium"/>
+          <eg-input width-type="medium" v-model="editRoleName"/>
+          <i class="iconfont icon-content_icon_required"/>
         </div>
         <div class="role-edit__row">
           <label class="role-edit__row-title">角色类型</label>
@@ -70,16 +71,26 @@
               :value="item.value"
             />
           </el-select>
+          <i class="iconfont icon-content_icon_required"/>
         </div>
         <div class="role-edit__row">
           <label class="role-edit__row-title align-top">权限菜单</label>
           <div class="role-edit__row-box">
+            <el-tree
+              ref="editTree"
+              :data="editTreeData"
+              node-key="value"
+              show-checkbox
+              default-expand-all
+              @check="handleCheck"
+            />
           </div>
+          <i class="iconfont icon-content_icon_required"/>
         </div>
         <div class="role-edit__row">
           <label class="role-edit__row-title"/>
-          <eg-button type="minor">取消</eg-button>
-          <eg-button>保存</eg-button>
+          <eg-button type="minor" @click="showEdit({isShow: false})">取消</eg-button>
+          <eg-button @click="saveClick">保存</eg-button>
         </div>
       </template>
     </eg-box>
@@ -108,7 +119,8 @@
         'isModify',
         'isShowEdit',
         'editRoleTypeList',
-        'editData'
+        'editData',
+        'editTreeData'
       ]),
       ...mapGetters([
       ]),
@@ -123,6 +135,10 @@
       editRoleType: {
         get () { return this.editData.RoleType },
         set (value) { this.updateObjectData({ obj: 'editData', item: 'RoleType', value }) }
+      },
+      editRoleName: {
+        get () { return this.editData.RoleName },
+        set (value) { this.updateObjectData({ obj: 'editData', item: 'RoleName', value }) }
       }
     },
     methods: {
@@ -130,21 +146,35 @@
         'showEdit',
         'getRoleListData',
         'getRoleType',
+        'getRoleMenus',
+        'addRoleData',
         'currentPageOnChange',
         'pageSizeOnChange',
         'updateStateData',
         'updateObjectData'
       ]),
       searchClick () {
-        this.getRoleType(true)
-        this.getRoleType(false)
         this.getRoleListData()
+      },
+      saveClick () {
+        if (this.isModify) {
+        } else {
+          this.addRoleData()
+        }
+      },
+      handleCheck (node, { checkedNodes, checkedKeys, halfCheckedNodes, halfCheckedKeys }) {
+        let totalNodes = [...checkedNodes, ...halfCheckedNodes]
+        // let Menus = totalNodes.map(menu => ({ Id: menu.value }))
+        this.updateObjectData({ obj: 'editData', item: 'Menus', value: totalNodes })
       }
     },
     watch: {
     },
     created () {
+      this.getRoleType(true)
       this.searchClick()
+      this.getRoleType(false)
+      this.getRoleMenus()
     }
   }
 </script>
--- src/views/main/system/role/role.js
+++ src/views/main/system/role/role.js
@@ -2,12 +2,17 @@ import Actions from '@/store/actions'
 import Mutations from '@/store/mutations'
 import * as types from '@/store/mutation-types'
 import api from '@/api'
+import initTree from '@/utils/tree'
+
+const TOTAL_OPTION_VALUE = 'total'
 
 const state = {
   reqCancels: new Map(),
   searchName: '',
-  searchTypeId: null,
-  searchRoleTypeList: [],
+  searchTypeId: TOTAL_OPTION_VALUE,
+  searchRoleTypeList: [
+    { label: '全部角色类型', value: TOTAL_OPTION_VALUE }
+  ],
   roleList: [],
   currentPage: 1,
   pageSize: 10,
@@ -20,7 +25,8 @@ const state = {
     RoleType: null,
     Menus: []
   },
-  editRoleTypeList: []
+  editRoleTypeList: [],
+  editTreeData: []
 }
 
 const getters = {
@@ -28,12 +34,26 @@ const getters = {
 
 const actions = {
   ...Actions,
-  showEdit ({ commit, state, getters, dispatch }, { isShow, row }) {
+  showEdit ({ commit, state, getters, dispatch }, { isShow = true, row } = { isShow: true }) {
     if (!isShow) {
+      // 清空表单
+      commit(types.SET_DATA, {
+        item: 'editData',
+        value: {
+          RoleId: null,
+          RoleName: '',
+          RoleType: null,
+          Menus: []
+        }
+      })
+      commit(types.SET_DATA, { item: 'editTreeData', value: [] })
     } else {
       if (row) {
+        // 编辑模式
         commit(types.SET_DATA, { item: 'isModify', value: true })
+        dispatch('getRoleMenus', row.CreatorId)
       } else {
+        // 添加模式
         commit(types.SET_DATA, { item: 'isModify', value: false })
       }
     }
@@ -46,12 +66,13 @@ const actions = {
       let data = res.Data || {}
       if (canEdit) {
         // 有编辑权限的角色列表
-        let searchRoleTypeList = Object.entries(data).map(([key, value]) => ({
+        let roleTypeList = Object.entries(data).map(([key, value]) => ({
           label: value,
-          value: key
+          value: parseInt(key)
         }))
+        let searchRoleTypeList = state.searchRoleTypeList.concat(roleTypeList)
         commit(types.SET_DATA, { item: 'searchRoleTypeList', value: searchRoleTypeList })
-        commit(types.SET_DATA, { item: 'searchTypeId', value: searchRoleTypeList.length ? searchRoleTypeList[0].value : null })
+        commit(types.SET_DATA, { item: 'searchTypeId', value: searchRoleTypeList[0].value })
       } else {
         // 创建角色的角色列表
         let editRoleTypeList = Object.entries(data).map(([key, value]) => ({
@@ -62,14 +83,18 @@ const actions = {
         commit(types.UPDATE_OBJ_DATA, { obj: 'editData', item: 'RoleType', value: editRoleTypeList.length ? editRoleTypeList[0].value : null })
       }
     }).catch(err => {
-      alert(err)
+      commit(types.CHECKOUT_FAILURE, err)
     })
   },
   getRoleListData ({ commit, state, getters, dispatch }) {
     let params = {
       q: state.searchName,
+      type: state.searchTypeId,
       pageSize: state.pageSize,
       pageIndex: state.currentPage
+    }
+    if (state.searchTypeId === null || state.searchTypeId === TOTAL_OPTION_VALUE) {
+      delete params.type
     }
     let getRoleListReq = api.role.getRoleList(params)
     commit(types.ADD_REQUEST_CANCEL, { item: 'getRoleListReq', value: getRoleListReq.cancel })
@@ -81,8 +106,34 @@ const actions = {
       commit(types.SET_DATA, { item: 'roleList', value: roleList })
       commit(types.SET_DATA, { item: 'totalCount', value: res.Count })
     }).catch(err => {
-      alert(err)
+      commit(types.CHECKOUT_FAILURE, err)
     }).finally(() => {
+    })
+  },
+  getRoleMenus ({ commit, state, getters, dispatch }, uid) {
+    let getRoleMenus = api.role.getRoleUserMenu(uid)
+    commit(types.ADD_REQUEST_CANCEL, { item: 'getRoleMenus', value: getRoleMenus.cancel })
+    getRoleMenus.request.then(res => {
+      let data = res.Data || []
+      let treeData = initTree(data)
+      commit(types.SET_DATA, { item: 'editTreeData', value: treeData })
+    }).catch(err => {
+      commit(types.CHECKOUT_FAILURE, err)
+    })
+  },
+  addRoleData ({ commit, state, getters, dispatch }) {
+    let editData = state.editData
+    let postData = {
+      RoleName: editData.RoleName,
+      RoleType: editData.RoleType,
+      Menus: editData.Menus.map(menu => ({ Id: menu.value }))
+    }
+    let addRoleData = api.role.postRoleData(postData)
+    commit(types.ADD_REQUEST_CANCEL, { item: 'addRoleData', value: addRoleData.cancel })
+    addRoleData.request.then(res => {
+      commit(types.CHECKOUT_SUCCEED, res.State)
+    }).catch(err => {
+      commit(types.CHECKOUT_FAILURE, err)
     })
   }
 }
