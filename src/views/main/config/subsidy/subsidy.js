6d1d275a5452eb86f32de1d5495acf889587cb0d
sihan.shan <850473661@qq.com>
2020-01-02 10:20:04 +0800

    #merge

--- src/views/main/config/subsidy/subsidy.js
+++ src/views/main/config/subsidy/subsidy.js
@@ -2,6 +2,7 @@ import Actions from '@/store/actions'
 import Mutations from '@/store/mutations'
 import * as types from '@/store/mutation-types'
 import api from '@/api'
+import initTree from '@/utils/tree'
 
 const state = {
   reqCancels: new Map(),
@@ -33,7 +34,20 @@ const state = {
     { value: 1, label: '按月' },
     { value: 2, label: '按季度' },
     { value: 3, label: '按学期' }
-  ]
+  ],
+  editData: {
+    Id: null,
+    Name: '',
+    SubType: 1,
+    IsClear: true,
+    Status: 0
+  },
+  pricePeriods: [{
+    Monney: null,
+    Days: []
+  }],
+  editRoomTree: [],
+  isLoadingEditRoomList: false
 }
 
 const getters = {
@@ -48,8 +62,10 @@ const actions = {
     }
     if (row) {
       commit(types.SET_DATA, { item: 'isModify', value: true })
+      dispatch('getEditRoomList', row.Id)
     } else {
       commit(types.SET_DATA, { item: 'isModify', value: false })
+      dispatch('getEditRoomList')
     }
     commit(types.SET_DATA, { item: 'isShowEdit', value: true })
   },
@@ -100,11 +116,36 @@ const actions = {
     }).finally(() => {
       commit(types.SET_LOADING_STATUS, { item: 'isLoadingRoomList', value: false })
     })
+  },
+  getEditRoomList ({ state, rootState, getters, commit }, id) {
+    let params = {
+      projectId: rootState.areaId,
+      taskType: 1
+    }
+    if (state.isModify) params.configId = id
+
+    let getEditRoomListReq = state.isModify ? api.group.getRoomConfigEditList(params) : api.group.getRoomConfigAddList(params)
+    commit(types.SET_LOADING_STATUS, { item: 'isLoadingEditRoomList', value: true })
+    commit(types.ADD_REQUEST_CANCEL, { item: 'getEditRoomListReq', value: getEditRoomListReq.cancel })
+    getEditRoomListReq.request.then(res => {
+      let data = res.Data || []
+      commit(types.SET_DATA, { item: 'editRoomTree', value: initTree(data, { rootLevel: 1 }) })
+    }).catch(err => {
+      commit(types.CHECKOUT_FAILURE, err)
+    }).finally(() => {
+      commit(types.SET_LOADING_STATUS, { item: 'isLoadingEditRoomList', value: false })
+    })
+  },
+  editPricePeriodDataOnChange ({ commit }, params) {
+    commit('UPDATE_PRICE_CONTENT', params)
   }
 }
 
 const mutations = {
-  ...Mutations
+  ...Mutations,
+  UPDATE_PRICE_CONTENT (state, { index, key, value }) {
+    state.pricePeriods[index][key] = value
+  }
 }
 
 export default {
